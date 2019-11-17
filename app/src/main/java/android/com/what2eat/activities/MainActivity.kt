d38efaf3bf45f9129e2225a5fc655e0a46cd33db
Nick Gysels <nick.gysels.w2819@student.hogent.be>
2019-11-17 15:42:02 +0100

    Add welcome fragment and MainActivity navigation

--- app/src/main/java/android/com/what2eat/activities/MainActivity.kt
+++ app/src/main/java/android/com/what2eat/activities/MainActivity.kt
@@ -1,10 +1,17 @@
-package android.com.what2eat
+package android.com.what2eat.activities
 
+import android.com.what2eat.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ *  MainActivity
+ */
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * ======= onCreate() =======
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/android/com/what2eat/fragments/WelcomeFragment.kt
@@ -0,0 +1,34 @@
+package android.com.what2eat.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import android.com.what2eat.R
+import android.com.what2eat.databinding.FragmentWelcomeBinding
+import androidx.databinding.DataBindingUtil
+
+/**
+ * Welcome Fragment
+ */
+class WelcomeFragment : Fragment() {
+
+    /**
+     * ======= onCreateView() =======
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // Layout Binding
+        val binding: FragmentWelcomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+
+        // Set Activity title
+        activity?.actionBar?.title = "Welcome"
+
+        return binding.root
+
+    }
+
+}
