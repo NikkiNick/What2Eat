880e71affdce9ba777eef2e700271a2950370dc3
Nick Gysels <nick.gysels.w2819@student.hogent.be>
2019-12-04 23:08:04 +0100

    Add recyclerview for MaaltijdOnderdelen on MaaltijdEdit +  removal functionality

---
+++ app/src/main/java/android/com/what2eat/adapters/MaaltijdOnderdeelRemoveAdapter.kt
@@ -0,0 +1,37 @@
+package android.com.what2eat.adapters
+
+import android.com.what2eat.databinding.MaaltijdonderdeelItemViewBinding
+import android.com.what2eat.databinding.MaaltijdonderdeelRemoveItemViewBinding
+import android.com.what2eat.model.MaaltijdOnderdeel
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MaaltijdOnderdeelRemoveAdapter(val clickListener: MaaltijdOnderdeelListener) : ListAdapter<MaaltijdOnderdeel, MaaltijdOnderdeelRemoveAdapter.ViewHolder>(MaaltijdOnderdeelDiffCallback()){
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position)!!, clickListener)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    class ViewHolder private constructor(val binding: MaaltijdonderdeelRemoveItemViewBinding): RecyclerView.ViewHolder(binding.root){
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = MaaltijdonderdeelRemoveItemViewBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+
+        fun bind(item: MaaltijdOnderdeel, clickListener: MaaltijdOnderdeelListener) {
+            binding.maaltijdOnderdeel = item
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
+        }
+    }
+}
