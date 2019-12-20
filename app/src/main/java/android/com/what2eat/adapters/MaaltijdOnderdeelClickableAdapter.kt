45ede3f3a171052a4ad62cb64dab497713eef7fe
Nick Gysels <nick.gysels.w2819@student.hogent.be>
2019-12-20 22:49:27 +0100

    Add MaaltijdOnderdelen overview (incl. navigation)

--- app/src/main/java/android/com/what2eat/adapters/MaaltijdOnderdeelClickableAdapter.kt
+++ app/src/main/java/android/com/what2eat/adapters/MaaltijdOnderdeelClickableAdapter.kt
@@ -1,35 +1,35 @@
 package android.com.what2eat.adapters
 
 import android.com.what2eat.databinding.MaaltijdonderdeelItemViewBinding
-import android.com.what2eat.generated.callback.OnClickListener
+import android.com.what2eat.databinding.MaaltijdonderdeelItemViewClickableBinding
 import android.com.what2eat.model.MaaltijdOnderdeel
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-
-class MaaltijdOnderdeelAdapter() : ListAdapter<MaaltijdOnderdeel, MaaltijdOnderdeelAdapter.ViewHolder>(MaaltijdOnderdeelDiffCallback()){
+class MaaltijdOnderdeelClickableAdapter(val clickListener: MaaltijdOnderdeelListener) : ListAdapter<MaaltijdOnderdeel, MaaltijdOnderdeelClickableAdapter.ViewHolder>(MaaltijdOnderdeelDiffCallback()){
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position)!!)
+        holder.bind(getItem(position)!!, clickListener)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
     }
 
-    class ViewHolder private constructor(val binding: MaaltijdonderdeelItemViewBinding): RecyclerView.ViewHolder(binding.root){
+    class ViewHolder private constructor(val binding: MaaltijdonderdeelItemViewClickableBinding): RecyclerView.ViewHolder(binding.root){
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = MaaltijdonderdeelItemViewBinding.inflate(layoutInflater, parent, false)
+                val binding = MaaltijdonderdeelItemViewClickableBinding.inflate(layoutInflater, parent, false)
                 return ViewHolder(binding)
             }
         }
 
-        fun bind(item: MaaltijdOnderdeel) {
+        fun bind(item: MaaltijdOnderdeel, clickListener: MaaltijdOnderdeelListener) {
             binding.maaltijdOnderdeel = item
+            binding.clickListener = clickListener
             binding.executePendingBindings()
         }
     }
