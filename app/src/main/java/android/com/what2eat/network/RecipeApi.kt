3fecaeed6b70d9a048e4d0dc3409e41fd73c014a
Nick Gysels <nick.gysels.w2819@student.hogent.be>
2019-12-25 21:01:06 +0100

    Dagger Retrofit API dependency injection

--- app/src/main/java/android/com/what2eat/network/RecipeApi.kt
+++ app/src/main/java/android/com/what2eat/network/RecipeApi.kt
@@ -8,9 +8,7 @@ import retrofit2.http.Query
 
 interface RecipeApi {
 
-    //@GET("search?app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_APP_KEY}")
-    //fun getRecipes(@Query("q") naam: String): Deferred<List<RecipeProperty>>
+    @GET("search?app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_APP_KEY}")
+    fun getRecipes(@Query("q") naam: String): Deferred<Response>
 
-    @GET("search?q=chicken&app_id=41e1b0e7&app_key=98495ccfce84506138e6735baafbdb49")
-    fun getRecipes(): Deferred<Response>
 }
